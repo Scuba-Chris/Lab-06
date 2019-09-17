653a21b9d4a5fae3ea79ac2ad4017156856909c6
Scuba-Chris <chrisceder@hotmail.com>
2019-09-17 10:36:58 -0700

    added to server

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # Lab-06
+
+Number and name of feature: Repo set up
+
+Estimate of time needed to complete: 20 mins
+
+Start time: 9:00
+
+Finish time: 10:15
+
+Actual time needed to complete: 1 hour 15
+
+Number and name of feature: Locations
+
+Estimate of time needed to complete: 1 hour
+
+Start time: 10:15
+
+Finish time: _____
+
+Actual time needed to complete: _____
---
+++ server.js
@@ -0,0 +1,44 @@
+'Use strict';
+
+Const express = require('express'); //library that allows server setup
+Const cors = require('cors');
+
+Const app = express(); //this initializes express server
+app.use(cors()) //tells browser that it is ok to communicate with the domain that the js isn’t running. This tells the browser that you want the client domain to be able to make server requests.
+
+require('dotenv').config(); //library that allows us to talk to .env file
+
+Const PORT = process.env.PORT || 3000 //(env file: PORT=3000)
+
+app.listen(PORT, () => console.log(`listening on ${PORT}`)); //this starts our express server
+
+// {
+//   "search_query": "seattle",
+//   "formatted_query": "Seattle, WA, USA",
+//   "latitude": "47.606210",
+//   "longitude": "-122.332071"
+// }
+
+app.get('/locations', request, response => {
+    //  get info from json data
+    // JSON data will be sent through constructor to create a lng lat format that is usable in order to create client side objects
+    try {
+        let searchQuery = request.query.data;
+        const geoDataResults = require('./data/geo.json');
+
+        const locations = new Location(searchQuery, geoDataResults);
+
+        response.status(200).send(locations);
+    } catch (err) {
+        console.error(err);
+    }
+
+    response.status(200).sent(results);
+})
+
+function Location(searchQuery, geoDataResults) {
+    this.searchQuery = searchQuery;
+    this.formatted_query = geoDataResults.results[0].formatted_address;
+    this.latitude = geoDataResults.results[0].geometry.location.lat;
+    this.longitude = geoDataResults.results[0].geometry.location.lng;
+}
