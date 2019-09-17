29cf52a3c3c5708aca276828d1a3a18c03c97efd
Scuba-Chris <chrisceder@hotmail.com>
2019-09-17 10:59:00 -0700

    server not working

--- server.js
+++ server.js
@@ -1,16 +1,15 @@
 'Use strict';
+//library that allows server setup
+const express = require('express');
+const cors = require('cors');
 
-Const express = require('express'); //library that allows server setup
-Const cors = require('cors');
-
-Const app = express(); //this initializes express server
-app.use(cors()) //tells browser that it is ok to communicate with the domain that the js isn’t running. This tells the browser that you want the client domain to be able to make server requests.
+const app = express(); //this initializes express server
+app.use(cors()); //tells browser that it is ok to communicate with the domain that the js isn’t running. This tells the browser that you want the client domain to be able to make server requests.
 
 require('dotenv').config(); //library that allows us to talk to .env file
 
-Const PORT = process.env.PORT || 3000 //(env file: PORT=3000)
+const PORT = process.env.PORT || 3000; //(env file: PORT=3000)
 
-app.listen(PORT, () => console.log(`listening on ${PORT}`)); //this starts our express server
 
 // {
 //   "search_query": "seattle",
@@ -20,25 +19,27 @@ app.listen(PORT, () => console.log(`listening on ${PORT}`)); //this starts our e
 // }
 
 app.get('/locations', request, response => {
-    //  get info from json data
-    // JSON data will be sent through constructor to create a lng lat format that is usable in order to create client side objects
-    try {
-        let searchQuery = request.query.data;
-        const geoDataResults = require('./data/geo.json');
+  //  get info from json data
+  // JSON data will be sent through constructor to create a lng lat format that is usable in order to create client side objects
+  try {
+    let searchQuery = request.query.data;
+    const geoDataResults = require('./data/geo.json');
 
-        const locations = new Location(searchQuery, geoDataResults);
+    const locations = new Location(searchQuery, geoDataResults);
+    response.status(200).send(locations);
+  } catch (err) {
+    console.error(err);
+  }
 
-        response.status(200).send(locations);
-    } catch (err) {
-        console.error(err);
-    }
-
-    response.status(200).sent(results);
+  response.status(200).sent(results);
 })
 
 function Location(searchQuery, geoDataResults) {
-    this.searchQuery = searchQuery;
-    this.formatted_query = geoDataResults.results[0].formatted_address;
-    this.latitude = geoDataResults.results[0].geometry.location.lat;
-    this.longitude = geoDataResults.results[0].geometry.location.lng;
+  this.searchQuery = searchQuery;
+  this.formatted_query = geoDataResults.results[0].formatted_address;
+  this.latitude = geoDataResults.results[0].geometry.location.lat;
+  this.longitude = geoDataResults.results[0].geometry.location.lng;
 }
+
+
+app.listen(PORT, () => console.log(`listening on ${PORT}`)); //this starts our express server
